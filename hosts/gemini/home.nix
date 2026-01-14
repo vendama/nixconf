1c804d410ffd636f9a05a8defe01f2c5232553f9
vendama <248530752+vendama@users.noreply.github.com>
2026-01-14 02:45:20 +0100

    fix steam not using breeze cursors

--- hosts/gemini/home.nix
+++ hosts/gemini/home.nix
@@ -21,5 +21,12 @@
     immutableByDefault = true;
   };
 
+  # Fix Steam not using breeze cursor
+  # Source: https://github.com/ValveSoftware/steam-for-linux/issues/11484
+  home.file.".local/share/icons/default" = {
+    source = "${pkgs.kdePackages.breeze}/share/icons/breeze_cursors";
+    recursive = true;
+  };
+
   home.stateVersion = "25.11";
 }
